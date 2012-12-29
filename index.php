6e46e0890d3690742f4aa77b75a38cd0047d0891
Jared Erickson <jared@lessmade.com>
2012-12-28 19:55:13 -0500

    initial build

---
+++ index.php
@@ -0,0 +1,200 @@
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>" />
+<meta name="viewport" content="width=device-width" />
+<title><?php wp_title( '|', true, 'right' ); ?></title>
+<link rel="profile" href="http://gmpg.org/xfn/11" />
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+
+<?php wp_head(); ?>
+
+</head>
+
+<body <?php body_class(); ?>>
+
+
+<?php
+	/*-----------------------------------------------------------------------------------*/
+	/* Start header
+	/*-----------------------------------------------------------------------------------*/
+?>
+
+<header id="masthead" class="site-header" role="banner">
+	<div class="container">
+		
+		<div class="gravatar">
+			<?php 
+				// grab admin email and their photo
+				$admin_email = get_option('admin_email');
+				echo get_avatar( $admin_email, 100 ); 
+			?>
+		</div><!--/ author -->
+	
+		<hgroup>
+			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a> &mdash; <span><?php echo get_bloginfo( 'description' ); ?></span></h1>
+		</hgroup>
+	
+		<nav role="navigation" class="site-navigation main-navigation">
+			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+		</nav><!-- .site-navigation .main-navigation -->
+		
+		<div class="clear"></div>
+	</div><!--/container -->
+		
+</header><!-- #masthead .site-header -->
+
+<div class="container">
+
+<?php
+	/*-----------------------------------------------------------------------------------*/
+	/* Start Home loop
+	/*-----------------------------------------------------------------------------------*/
+	
+	if( is_home() ) {
+	
+?>
+		<div id="primary">
+			<div id="content" role="main">
+
+			<?php if ( have_posts() ) : ?>
+
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<article class="post">
+					
+						<h1 class="title">
+							<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+								<?php the_title() ?>
+							</a>
+						</h1>
+						<div class="post-meta">
+							<?php if( comments_open() ) : ?>
+								<span class="comments-link">
+									<?php comments_popup_link( __( 'Comment', 'break' ), __( '1 Comment', 'break' ), __( '% Comments', 'break' ) ); ?>
+								</span>
+							<?php endif; ?>
+						
+						</div><!--/post-meta -->
+						
+						<div class="the-content">
+							<?php the_content( 'Continue...' ); ?>
+							
+							<?php wp_link_pages(); ?>
+						</div><!-- the-content -->
+						
+						<div class="category clearfix">
+							<?php echo get_the_category_list(); ?>
+						</div>
+						
+					</article>
+
+				<?php endwhile; ?>
+				
+				<!-- pagintation -->
+				<div id="pagination" class="clearfix">
+					<div class="past-page"><?php previous_posts_link( 'Newer &raquo;' ); ?></div>
+					<div class="next-page"><?php next_posts_link( ' &laquo; Older' ); ?></div>
+				</div><!-- pagination -->
+
+
+			<?php else : ?>
+				
+				<article class="post error">
+					<h1 class="404">Nothing posted yet</h1>
+				</article>
+
+			<?php endif; ?>
+
+			</div><!-- #content .site-content -->
+		</div><!-- #primary .content-area -->
+		
+	<?php } //end is_home(); ?>
+
+<?php
+	/*-----------------------------------------------------------------------------------*/
+	/* Start Single loop
+	/*-----------------------------------------------------------------------------------*/
+	
+	if( is_single() || is_archive() ) {
+?>
+
+	
+		<div id="primary">
+			<div id="content" role="main">
+
+			<?php if ( have_posts() ) : ?>
+
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<article class="post">
+					
+						<h1 class="title"><?php the_title() ?></h1>
+						<div class="post-meta">
+							<?php if( comments_open() ) : ?>
+								<span class="comments-link">
+									<?php comments_popup_link( __( 'Comment', 'less' ), __( '1 Comment', 'less' ), __( '% Comments', 'less' ) ); ?>
+								</span>
+							<?php endif; ?>
+						
+						</div><!--/post-meta -->
+						
+						<div class="the-content">
+							<?php the_content( 'Continue...' ); ?>
+							
+							<?php wp_link_pages(); ?>
+						</div><!-- the-content -->
+						
+					</article>
+
+				<?php endwhile; ?>
+				
+				<?php
+					// If comments are open or we have at least one comment, load up the comment template
+					if ( comments_open() || '0' != get_comments_number() )
+						comments_template( '', true );
+				?>
+
+
+			<?php else : ?>
+				
+				<article class="post error">
+					<h1 class="404">Nothing posted yet</h1>
+				</article>
+
+			<?php endif; ?>
+
+			</div><!-- #content .site-content -->
+		</div><!-- #primary .content-area -->
+
+	<?php } //end is_single(); ?>
+	
+<?php
+	/*-----------------------------------------------------------------------------------*/
+	/* Start Page loop
+	/*-----------------------------------------------------------------------------------*/
+	
+	if( is_page()) {
+?>
+	<?php } // end is_page(); ?>
+<?php
+	/*-----------------------------------------------------------------------------------*/
+	/* Start Footer
+	/*-----------------------------------------------------------------------------------*/
+?>
+
+</div><!-- / container-->
+
+	<footer class="site-footer" role="contentinfo">
+		<div class="site-info container">
+			<?php do_action( 'break_credits' ); ?>
+			<a href="http://wordpress.org/" title="A Semantic Personal Publishing Platform" rel="generator">Proudly powered by WordPress</a>
+			<span class="sep"> and </span>
+			<a href="http://lessmade.com/themes/less" rel="theme">LESS</a> by <a href="http://jarederickson.com" rel="designer">Jared Erickson</a>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon .site-footer -->
+</div><!-- #page .hfeed .site -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
