25751fb6ac529faef000eac232cd25e28f94bad0
Jared Erickson <jared@lessmade.com>
2012-12-31 18:08:11 -0500

    updated header to include a #brand div in place of the hgroup

--- index.php
+++ index.php
@@ -43,8 +43,10 @@
 				echo get_avatar( $admin_email, 100 ); 
 			?>
 		</div><!--/ author -->
-	
-		<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a> &mdash; <span><?php echo get_bloginfo( 'description' ); ?></span></h1>
+		
+		<div id="brand">
+			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a> &mdash; <span><?php echo get_bloginfo( 'description' ); ?></span></h1>
+		</div><!-- /brand -->
 	
 		<nav role="navigation" class="site-navigation main-navigation">
 			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
