9ba57e77e5211054d514eb5e51dc1f78ea98b1b0
Jeremy Herve <jeremy@tagada.hu>
2013-06-08 11:19:15 +0200

    Add search results loop
    Avoid blank page if a logged in user uses the built-in admin bar
    search, or if a reader hacks the URL to search for something (?s=)

--- index.php
+++ index.php
@@ -49,6 +49,76 @@
 	<div id="primary">
 		<div id="content" role="main">
 
+<?php
+	/*-----------------------------------------------------------------------------------*/
+	/* Start Search results loop
+	/*-----------------------------------------------------------------------------------*/
+	
+	if( is_search() ) {
+	
+?>
+
+			<?php if ( have_posts() ) : ?>
+				
+				<header class="page-header">
+					<h1 class="page-title"><?php printf( __( 'Search Results for: %s' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
+				</header><!-- .page-header -->
+
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<article class="post">
+					
+						<h1 class="title">
+							<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+								<?php the_title() ?>
+							</a>
+						</h1>
+						<div class="post-meta">
+							<?php if( comments_open() ) : ?>
+								<span class="comments-link">
+									<?php comments_popup_link( __( 'Comment', 'break' ), __( '1 Comment', 'break' ), __( '% Comments', 'break' ) ); ?>
+								</span>
+							<?php endif; ?>
+						
+						</div><!--/post-meta -->
+						
+						<div class="the-content">
+							<?php the_content( 'Continue...' ); ?>
+							
+							<?php wp_link_pages(); ?>
+						</div><!-- the-content -->
+						
+						<div class="meta clearfix">
+							<div class="category"><?php echo get_the_category_list(); ?></div>
+							<div class="tags"><?php echo get_the_tag_list( '| &nbsp;', '&nbsp;' ); ?></div>
+						</div><!-- Meta -->
+						
+					</article>
+
+				<?php endwhile; ?>
+				
+				<!-- pagintation -->
+				<div id="pagination" class="clearfix">
+					<div class="past-page"><?php previous_posts_link( 'Newer &raquo;' ); ?></div>
+					<div class="next-page"><?php next_posts_link( ' &laquo; Older' ); ?></div>
+				</div><!-- pagination -->
+
+
+			<?php else : ?>
+				
+				<article class="post error">
+				
+					<h1 class="404"><?php _e( 'Nothing Found' ); ?></h1>
+					
+					<div class="the-content">
+						<p><?php _e( 'Sorry, but nothing matched your search terms.' ); ?></p>
+					</div><!-- the-content -->
+					
+				</article>
+
+			<?php endif; ?>	
+
+	<?php } //end is_search(); ?>
 
 <?php
 	/*-----------------------------------------------------------------------------------*/
