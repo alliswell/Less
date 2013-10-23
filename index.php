43d05e8bc75166e4bd1c4ef76399aeef178109c5
Jeremy Herve <jeremy@tagada.hu>
2013-10-23 11:50:54 +0000

    Add a loop to handle attachment pages

--- index.php
+++ index.php
@@ -186,7 +186,7 @@
 	/* Start Single loop
 	/*-----------------------------------------------------------------------------------*/
 	
-	if( is_single() ) {
+	if( is_single() && ! is_attachment() ) {
 ?>
 
 
@@ -238,6 +238,81 @@
 
 
 	<?php } //end is_single(); ?>
+
+<?php
+	/*-----------------------------------------------------------------------------------*/
+	/* Start Attachment loop
+	/*-----------------------------------------------------------------------------------*/
+	
+	if( is_attachment() ) {
+?>
+
+			<?php if ( have_posts() ) : ?>
+
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<article class="post">
+					
+						<h1 class="title"><?php the_title() ?></h1>
+						<div class="post-meta">
+							<?php if( comments_open() ) : ?>
+								<span class="comments-link">
+									<?php comments_popup_link( __( 'Comment', 'less' ), __( '1 Comment', 'less' ), __( '% Comments', 'less' ) ); ?>
+								</span>
+							<?php endif; ?>
+						
+						</div><!--/post-meta -->
+						
+						<div class="the-content">
+
+							<?php echo wp_get_attachment_image( $post->ID, 'full' ); ?>
+
+							<?php if ( ! empty( $post->post_excerpt ) ) : ?>
+								<div class="entry-caption">
+									<?php the_excerpt(); ?>
+								</div>
+							<?php endif; ?>
+
+						</div><!-- the-content -->
+
+						<div class="meta clearfix">
+							<?php
+								$metadata = wp_get_attachment_metadata();
+								printf( __( '<span class="meta-prep meta-prep-entry-date">Published </span> <span class="entry-date"><abbr class="published" title="%1$s">%2$s</abbr></span> at <a href="%3$s" title="Link to full-size image">%4$s &times; %5$s</a> in <a href="%6$s" title="Return to %7$s" rel="gallery">%8$s</a>', 'less' ),
+									esc_attr( get_the_time() ),
+									get_the_date(),
+									esc_url( wp_get_attachment_url() ),
+									$metadata['width'],
+									$metadata['height'],
+									esc_url( get_permalink( $post->post_parent ) ),
+									esc_attr( strip_tags( get_the_title( $post->post_parent ) ) ),
+									get_the_title( $post->post_parent )
+								);
+							?>
+						</div><!-- Meta -->						
+						
+					</article>
+
+				<?php endwhile; ?>
+				
+				<?php
+					// If comments are open or we have at least one comment, load up the comment template
+					if ( comments_open() || '0' != get_comments_number() )
+						comments_template( '', true );
+				?>
+
+
+			<?php else : ?>
+				
+				<article class="post error">
+					<h1 class="404">Nothing posted yet</h1>
+				</article>
+
+			<?php endif; ?>
+
+
+	<?php } //end is_attachment(); ?>
+
 	
 <?php
 	/*-----------------------------------------------------------------------------------*/
